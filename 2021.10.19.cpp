7ee557602d09fe831b992ac337dac14e0b63fa5d
AIQIYI1 <90768427+AIQIYI1@users.noreply.github.com>
2021-10-19 11:21:26 +0800

    Add files via upload

---
+++ 2021.10.19.cpp
@@ -0,0 +1,111 @@
+#include<stdio.h>
+//int main(){
+/*	int age ;
+	double salary=4000.0;
+	scanf("%d",&age);
+	if (age>60)
+	{
+		salary=salary*1.5;
+	}
+	else if(age>50)
+	{
+		salary=salary*1.2;
+	}
+	else if(age>30)
+	{
+	salary=salary*1.7;		
+	}
+	else 
+	{
+		salary=salary;
+	}
+	printf("您的工资为%f",salary);
+*/
+/*
+switch case 语句
+switch(控制表达式){ 
+
+case 常量:
+语句
+……
+
+case 常量:   要有空格才行 
+语句
+……
+
+default: 
+语句
+…… 
+} 
+*/
+/* 
+int main(){
+ int type;
+ printf("请输入类型\n",type);
+ scanf("%d",&type);
+ 
+ switch(type){
+ 	case 1:
+ 		printf("你好呀\n");
+ 		break;
+ 	case 2:
+	 printf("最近怎么样啊\n");
+	 break;
+	case 3:
+	printf("吃饱了吗？\n");
+	break;
+	default:
+	printf("平安喜乐\n");
+	break; 	
+ }
+	return 0;
+} 
+*/
+//switch-case 分段函数 
+/* 
+int main(){
+	int x,y,type;
+	scanf("%d",&x);
+    int a,b;
+	a== (x>=0),b ==( x=0);
+    type = a+b;/*x大于0，则type=1,=0,则=2  小于0，则=1*/ 
+//	switch(type){
+//		case 1:
+//		printf("y=1\n");
+//		break;
+//		case 2:
+//		printf("y=0\n");
+//		case 0:
+//		printf("y=-1\n");
+//		break;
+//	}
+//	return 0;
+//}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
