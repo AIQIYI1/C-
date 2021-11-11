b70fc1ff19353cb0945e7741a9f033bece9fe7d4
AIQIYI1 <90768427+AIQIYI1@users.noreply.github.com>
2021-11-11 19:37:52 +0800

    Add files via upload

--- oj.cpp
+++ oj.cpp
@@ -1,5 +1,5 @@
 #include<stdio.h>
-
+/*
 
 int main(){
 	int a,b,c,d,e,m,x,i,p,q;
@@ -63,7 +63,7 @@ int main(){
 	}
 	return 0;
 }
-
+*/
 /*
 int main(){
 int a=0,b=0;
@@ -86,7 +86,101 @@ int main(){
 }*/
 
 
+/*判断直角三角形 
+int main(){
+	int n,a,b,c,max,min,minn;
+	
+	scanf("%d",&n);
+	while(n--){
+		scanf("%d%d%d",&a,&b,&c);
+		if(a>b&&b>c){
+			max=a;
+			min=b;
+			minn=c;
+		}else if(b>a&&a>c){
+			max=b;
+			min=a;
+			minn=c;
+		}else if(c>b&&b>a){
+			max=c;
+			min=b;
+			minn=a;
+		}
+		if(max*max==min*min+minn*minn){
+			printf("Yes\n");
+		}else{
+			printf("No\n");
+		}
+	} 
+	
+	return 0;
+}
+*/
 
+
+//水仙花数 
+/* 
+int main(){
+	int x,m,n,a,b,c,i,count=0;
+	scanf("%d",&x);
+	while(x--){
+	scanf("%d%d",&m,&n);
+	
+	for(i=m;i<=n;i++){
+		a=i%10;
+		b=(i/10)%10;
+		c=i/100;
+		if(a*a*a+b*b*b+c*c*c==i){
+			printf("%d ",i);
+			count++;
+		}
+	}
+	if(count==0){
+		printf("-1\n");
+	} //所以应该放在while循环内，for循环外 
+}
+//if(count==0){
+//	printf("-1\n");
+//}if 放在外边的时候，是根据两组数据来判断的，其中一组数据有水仙花数都不会出现-1 
+	return 0;
+} 
+*/
+
+int main(){
+	int x,a=3,b=3,m=1,n=2,p=1,i=2;
+	
+	scanf("%d",&x);
+	if(x<=6||x%2!=0){
+		printf("ERROR\n");
+	}
+	else{
+		while(a<=(x/2)){
+			for(i=2,m=1;i<a;i++){
+				if(a%i==0){
+					m=0;
+						break;//break 放在if里才是发现它非素数时跳出循环，如果放在if外for里面，则整个循环只会进行一次，并不是所有的i都会被用到 
+				}
+			
+			
+			}
+				b=x-a;
+			for(n=2,p=1;n<b;n++){
+				if(b%n==0){
+				p=0;
+				break;
+				}
+				
+			}
+			
+			if(m*p!=0){
+				printf("%d %d\n",a,b);
+			}
+				a++;
+		}	
+		}
+	
+	return 0;
+}
 
 
 
