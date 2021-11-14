3000232fc3894b773d6dca12a4ddee968d5e1c1a
AIQIYI1 <90768427+AIQIYI1@users.noreply.github.com>
2021-11-14 23:34:56 +0800

    Add files via upload

---
+++ textbook3.cpp
@@ -0,0 +1,71 @@
+#include<stdio.h>
+int main(){
+	/*
+int main()
+{
+	char a,b,c;
+	a=getchar();
+	b=getchar();
+	c=getchar();
+	putchar(a);
+	putchar(b);
+	putchar(c);
+	putchar('\n');
+	return 0;
+}
+*/
+//大小写的字母转换 
+/*int main(){
+
+char c1,c2;
+c1=getchar();
+c2=c1+32;
+putchar(c2);
+putchar('\n');
+
+return 0;
+}
+*/
+//替换字符成字母后的第4位；
+/* 
+char c1='C',c2='h',c3='i',c4='n',c5='a';
+c1=c1+4;c2=c2+4;c3=c3+4;c4=c4+4;c5=c5+4;
+putchar(c1);
+putchar(c2);
+putchar(c3);
+putchar(c4);
+putchar(c5);
+putchar('\n');
+
+printf("%c%c%c%c%c",c1,c2,c3,c4,c5);
+*/
+
+	return 0;
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
