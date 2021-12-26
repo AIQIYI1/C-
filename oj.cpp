ced6285ddd519045bb9bd1abc778156e5ee06f21
AIQIYI1 <90768427+AIQIYI1@users.noreply.github.com>
2021-12-26 20:58:49 +0800

    Add files via upload

--- oj.cpp
+++ oj.cpp
@@ -1,7 +1,9 @@
+#include<ctype.h>
 #include<stdio.h>
 #include<string.h>
-/*
+#include<stdlib.h>
 
+/*
 int main(){
 	int a,b,c,d,e,m,x,i,p,q;
 	scanf("%d%d%d%d%d",&a,&b,&c,&d,&e);
@@ -826,21 +828,6 @@ int main() {
 	return 0;
 }
 */
-/*字符串编码 
-int main(){
-char str[100];
-int m,i=0;
-gets(str);
-m=strlen(str);
-for(int i=0;i<m;i++){
-	if(str[i]!=' '&&str[i]<90){
-	str[i]=155-str[i];
-}
-}
-puts(str);
-	return 0;
-}
-*/
 
 //分解质因数
 /* 
@@ -962,22 +949,6 @@ int main(){
 //	return 0;
 //} 
 
-//日期距离 
-/*
-int main(){
-	char *a[12]={"January","February","March","April","May","June","July","August","September","October","November","December"};
-	for(int i=0;i<12;i++){
-		printf("%s",a[i]);
-	}//给相应的月份命名；
-	
-	char b[20];
-	for(int j=0;j<8;j++) {
-		scanf("%c",y&b[j]);
-	}
-	
-	return 0;
-}
-*/
 //分类排序
 //int  main(){
 //void sort(int a[],int m);
@@ -1179,7 +1150,7 @@ int main(){
 // return 0; 
 //
 //}
-#include<stdlib.h>
+//#include<stdlib.h>
 //int main(){
 //	struct Student{
 //		char name[11];
@@ -1219,7 +1190,8 @@ int main(){
 //	return 0;
 //} 
 
- 
+ //查找2 
+ /*
 int main(){
 	struct infor{
 		char name[10];
@@ -1232,11 +1204,10 @@ int main(){
 	int n,i,p=0;
 	char str[10];
 	char a[4]="n/a";
-	char tempt1[4];
 	scanf("%d %s",&n,str);
-	
+	//输入信息 
 	for(i=0;i<n;i++){
-		scanf("%s %s %s",&students[i].name,&students[i].num,&students[i].score);//输入信息 
+		scanf("%s %s %s",&students[i].name,&students[i].num,&students[i].score);
 	}
 //	for(i=0;i<n;i++){
 //		if(strcmp(students[i].score,a)!=0){
@@ -1253,39 +1224,39 @@ int main(){
 				students[i].rank++;
 			}
 	}
-	
 	}
-		for(int k=0;k<n;k++){ 
-		printf("%s %s %s %d\n",students[i].name,students[i].num,students[i].score,students[i].rank);
-	}
-	/*
+//		for(int k=0;k<n;k++){ 
+//		printf("%s %s %s %d\n",students[k].name,students[k].num,students[k].score,students[k].rank);
+//	}
 	//开始查找 
-	//如果是分数 
-	 if(atoi(str)!=0||strcmp(str,a)==0){
-	 	for(i=0;i<n;i++){
-	 		if(atoi(students[i].score)==atoi(str)){
+	//如果是分数 0&n/a
+	if(atoi(str)<=100&&atoi(str)>0||atoi(str)==0&&strlen(str)==1||strcmp(str,a)==0){
+	 if(atoi(str)<=100&&atoi(str)>0||atoi(str)==0&&strlen(str)==1){
+	 			for(i=0;i<n;i++){
+	 				if(atoi(str)==atoi(students[i].score)&&strcmp(students[i].score,a)!=0)
 	 			printf("%s %s %s %d\n",students[i].name,students[i].num,students[i].score,students[i].rank);
 			 } 
-		 } 
-	 }
-	 if(strcmp(str,a)==0){
+		 }
+	 else if(strcmp(str,a)==0){
 	 	for(i=0;i<n;i++){
 	 		if(strcmp(students[i].score,a)==0){
 	 			printf("%s %s %s n/a\n",students[i].name,students[i].num,students[i].score);
 			 } 
 		 }
-	 }
+	 } 
+}
 	 //模糊查找 
 	 else{
-	 	
 	for(int k=0;k<n;k++){ //进入了某个人的信息 
-	if(strstr(str,students[k].name)!=0||strstr(str,students[k].num)!=0){
-		printf("%s %s %s %d\n",students[i].name,students[i].num,students[i].score,students[i].rank);
+	if(strstr(students[k].name,str)!=0||strstr(students[k].num,str)!=0){
+	//	printf("null");
+		printf("%s %s %s %d\n",students[k].name,students[k].num,students[k].score,students[k].rank);
 	}
-}}*/
+  }
+}
 	return 0;
 }
-
+*/
 //含无效项的平均数 
 //int main(){
 //	char num[100][100];
@@ -1314,24 +1285,562 @@ int main(){
 
 
 //日期距离
-/*int main (){
+ //忽略了月份小于12的时候完全对称的时候，只有一种情况，就直接输出日子 
+//一方面要考虑第一个数能不能是月份（12为临界值），另一方面 
+/*
+#include<stdio.h>
+#include<string.h>
+int main(){
+	int runyear(int m);
+	int max(int m,int n);
+	int min(int m,int n);
+	char*monthname[13]={"January", "February","March","April","May","June","July","August","September","October","November","December"};
+	int days[13]={31,28,31,30,31,30,31,31,30,31,30,31};
+	int year=0,month=0,day=0,year1=0,month1=0,day1=0;
+	char date[20];
+	int past=0,past1=0;
+	int m=0,sum=0,j=0;
+	scanf("%s",date);
+	sscanf(date,"%d/%d/%d",&year,&month,&day);//先考虑唯一的情况 （两个表示方法表示同一天；只能是年月日——第一个数大于12；
+	//只能是月日年——第二个数>12或者第三个数>相应月份的的天数或者第二个数是2第三个数是29但是年不是闰年） 
+//	printf("%d %d %d",year,day,month);
+
+//年月日法 
+	if(year>12||year==month&&month==day){
+		printf("%s %d,%d",monthname[month-1],day,2000+year);//关于要不要加*的问题！！ 
+	}
 	
-/*	char a[3];
-	char b[3];
-	char c[3];
-	char*month[12]={"January", "February","March","April","May","June","July","August","September","October","November","December"};
-	int m;
-	scanf("%s/%s/%s",a,b,c);
-	if(m=atoi(b)>12){
-		if(b[0]==0&&c[0]==0) printf("%s %c,20%d%d",month[m-1],b[1],a[0],a[1]);
-		if(b[0]!=0&&c[0]==0) printf("%s %c%c,20%d%d",month[m-1],b[0],b[1],a[0],a[1]);
-		if(b[0]==0&&c[0]!=0) printf("%s %c,20%d%d",month[m-1],b[1],a[0],a[0],a[1]);
-		if(b[0]!=0&&c[0]!=0) printf("%s %c%c,20%d%d",month[m-1],b[0],b[1],a[0],a[1]);
+	//月日年法 
+	else if(month>12||day>days[month-1]||month==2&&runyear(2000+year)!=1&&day==29){
+	sscanf(date,"%d/%d/%d",&month1,&day1,&year1);
+	printf("%s %d,%d",monthname[month1-1],day1,2000+year1);
+}
+//再考虑两种都可以的情况——要满足两种情况之一 
+ else  if(month<12&&day<=days[month-1]||year<=12&&month<=days[month-1]){
+ 	sscanf(date,"%d/%d/%d",&month1,&day1,&year1);
+ 		int count=0;//在两个年份之间的闰年数目 (先不考虑头尾两年)
+	for(int i=min(year,year1)+1;i<max(year,year1);i++){
+		if(runyear(2000+i)==1) count++;
+	}
+	sum=count*366+(max(year,year1)-min(year,year1)-1-count)*365; 
+	//分别将两个年份分割成过去的和没过去的两部分
+	m=days[1];
+	days[1]=days[1]+runyear(2000+year);
+	for(j=0;j<month-1;j++){
+		past+=days[j];
+	} 
+	past=past+day;
+	days[1]=m+runyear(2000+year1);
+	for(j=0;j<month1-1;j++){
+		past1+=days[j];
+	} 
+	past1=past1+day1;
+	//比较两个年份的先后
+	
+	if (year>year1){
+    sum=sum+365+runyear(2000+year)-past1+past;
+	}
+	else if (year<year1){
+	sum=sum+365+runyear(2000+year1)-past+past1;
+	}
+	else if(year1=year) {
+		sum=past-past1;
+		if(sum>=0) sum=sum;
+		else sum=-sum;
+	}
+		printf("%d",sum);
+}
+return 0;
+}
+int runyear(int m){
+	int flag=0;
+	if(m%400==0||m%4==0&&m%100!=0) flag=1;
+	return flag;
+}
+int max(int m,int n){
+	int max=m;
+	if(n>m) max=n;
+	return max;
+}
+
+int min(int m,int n){
+	int min=m;
+	if(n<m) min=n;
+	return min;
+}
+*/
+
+//中值滤波
+//思路： 输入数据 ：m表示滤波宽度，n表示数组长度
+//首先排除ERROR的情况，再将符合情况的数组从 m/2 项到n-m/2 开始进入sort函数进行替换 
+//注意不能直接在原数组上面改动，会影响后续操作——创建一个一样的数组 
+ /*
+int main(){
+	int sort(int a[],int m);
+	int m,n,i=0,j=0,k=0;
+	int a[50],num[50],b[50];
+	
+	while(scanf("%d%d",&m,&n)!=EOF){
+			for(i=0;i<n;i++){
+				scanf("%d",&a[i]);
+				b[i]=a[i]; 
+			}
+			if(m%2==0) printf("ERROR\n");
+			else{
+			for(i=m/2;i<n-m/2;i++){
+				for(j=0,k=i-m/2;j<m&&k<=i+m/2;j++){
+					num[j]=a[k];
+					k++;
+				}
+			b[i]=sort(num,m);
+			}
+			for(i=0;i<n;i++){
+				printf("%d ",b[i]);
+			}
+		}
+	}
+	return 0;
+}
+//降序排列 
+int sort(int a[],int m){
+	int i=0,j=0,tempt=0,t;
+	int b[20];
+	for(i=0;i<m;i++){
+		b[i]=a[i];
+	}
+	for(i=0;i<m;i++){
+		for(j=i+1;j<m;j++){
+			if(b[i]<b[j]){
+				tempt=b[i];
+				b[i]=b[j];
+				b[j]=tempt; 
+			}
+		}
+	}
+	t=b[m/2];
+	return t;
+}*/
+
+
+//#include<stdio.h>
+//#include<string.h>
+//int main(){
+//char str[100];
+//int m,i=0;
+//gets(str);
+//m=strlen(str);
+//for(int i=0;i<m;i++){
+//	if(str[i]<90&&str[i]>=65){
+//	str[i]=155-str[i];
+//}
+//}
+//puts(str);
+//printf("\n");
+//	return 0;
+//}
+
+//柱状图
+//思路：接受数字和字符后，得到整个图的行数和列数；（得到正负半轴的最大值，相加后和再加1） 
+//先将整个图的每个位置置为“* ”
+//对正负半轴分别进行处理，得到 各自的存数字和字符的数组 
+/* 
+#include<stdio.h>
+int main()
+{
+	int m, i = 0, s[999], q = 0, c[999], d[999], e = 0, f = 0, h, g, max = 0, fumax = 0, zong, hang, lie;
+	char ch[999], b = 0;
+	scanf("%d", &m);
+	char lan[50][50];
+
+	for (i = 0; i < 999; i++) {
+		ch[i] = '*';
+	}
+	int j = 0;
+	for (i = 0; i < 50; i++) {
+		for (int j = 0; j < 50; j++) {
+			lan[i][j] = '*';
+		}
+	}
+//先将图中所有的位置给初始化为‘*’ 
+	for (i = 0; i < m; i++)
+	{
+		scanf(" %d%c", &s[i], &ch[i]);//分别接受输入的数字和字符 
+		if (ch[i] == '\n')
+			ch[i] = ' ';
+		if (s[i] > 0)
+		{
+			c[e] = s[i];
+			e++;
+		}
+		if (s[i] <= 0)
+		{
+			d[f] = -s[i];
+			f++;
+		}
+	}
+//分别得到正负半轴要打字符的地方 ，此时得到的e是正半轴有字符的列数 
+	if (f != 0)
+		fumax = d[0];
+	if (f == 0)
+		fumax = 0;
+	for (h = 1; h < e; h++)
+	{
+		if (c[h] >= max)
+			max = c[h];
+	}
+	for (g = 1; g < f; g++)
+	{
+		if (d[g] >= fumax)
+			fumax = d[g];
+	}
+	//得到正负半轴 字符最高峰 
+
+	zong = max + fumax + 1;
+	for (lie = 0; lie < 2 * m - 1; lie++)
+	{
+		lan[max][lie] = '-';
+	}
+	//打印x轴 
+	for (i = 0; i < m; i++)
+	{
+//lan的排列是从正半轴的最高峰所在行开始的，但是排列显示的时候从x轴向两边排列 
+		if (ch[i] == ' ' && s[i] > 0)
+		{
+			for (hang = max - 1; hang >= max - s[i]; hang--)
+				lan[hang][2 * i] = '+';
+		}
+		 //i*2是偶数，对应的是奇数列 
+		if (ch[i] == ' ' && s[i] < 0)
+		{
+			for (hang = max + 1; hang <= max - s[i]; hang++)
+				lan[hang][2 * i] = '+';
+		}
+		if (ch[i] != ' ' && s[i] > 0)
+		{
+			for (hang = max - 1; hang >= max - s[i]; hang--)
+				lan[hang][2 * i] = ch[i];
+		}
+		if (ch[i] != ' ' && s[i] < 0)
+		{
+			for (hang = max + 1; hang <= max - s[i]; hang++)
+				lan[hang][2 * i] = ch[i];
+		}
+	}
+	for (hang = 0; hang < zong; hang++)
+	{
+		for (lie = 0; lie < 2 * m - 1; lie++)
+		{
+			if (lan[hang][lie] == '*') {
+				printf(" ");
+			}
+			else {
+				printf("%c", lan[hang][lie]);
+			}
+
+
+		}
+		printf("\n");
 	}
 
 	return 0;
 }
 */
+//A blockhouse is a small castle that has four openings through which to shoot.
+/* 
+int main(){
+    void sort(char name[][100], int n);
+    int i, count=0, j=0;
+    char word[1000][100], str[1000];
+    char *p;
+    while(scanf("%s",str)!=EOF)//输入字符串
+    p=str;// 将输入值得地址赋值给指针
+    for(;*p!=0; p++){
+        if(*p!=' ' && *p != ',' &&  *p != '.'){//分割单词
+            if(*p>='A'&& *p<='Z') //大写变小写
+               *p += 32;
+            word[count][j++]=*p;
+        }
+        //遇到空格、逗号、句号
+        else{
+            if(j!=0){
+                word[count][j++]='\0';//单词结束标志
+                count++;
+                j=0;
+            }
+        }
+    }
+    //将分割后的单词排序
+    sort(word,count);
+    int sum = 1;
+    for(i=0;i<count; i++){
+        if(strcmp(word[i],word[i+1])== 0)
+            sum++;
+        else{
+            printf("%s:%d\n", word[i],sum);
+            sum = 1;
+        }
+         
+    }
+    return 0;
+}
+ 
+void sort(char name[][100], int n){
+    char temp[100];
+    int i, j, k;
+    for(i=0; i<n-1; i++){
+        k=i;
+        for(j=i+1; j<n; j++){
+            if(strcmp(name[k], name[j]) > 0) k=j;
+        }
+        if(k != i){
+            strcpy(temp, name[i]);
+            strcpy(name[i], name[k]);
+            strcpy(name[k], temp);
+        }
+    }
+ 
+}
+*/
+//矩阵的幂
+//思路：先输入要求的数字n,k次方，二维数组A赋值，进行赋值，
+//并且创建相同的二维数组 B，创建C用于后面中间过渡 
+//
+/*
+int main(){
+	int k,n;
+	scanf("%d%d",&n,&k);
+	int A[50][50],B[50][50],C[50][50];
+	int i=0,j=0,sum=0,a=0,count=0;
+	//对A进行赋值 
+	for(i=0;i<n;i++){
+		for(j=0;j<n;j++){
+			scanf("%d",&A[i][j]);
+			B[i][j]=A[i][j];
+		}
+	} 
+	//开始进行计算
+	int b=k-1;
+	while(b--){
+		for(i=0;i<n;i++){
+			for(j=0;j<n;j++){
+				sum=0;
+				a=0;
+				while(a<n){
+				sum+=A[i][a]*B[a][j];
+				a++;
+				}
+				C[i][j]=sum;
+			}
+		}
+		for(i=0;i<n;i++){
+		for(j=0;j<n;j++){
+			B[i][j]=C[i][j]; 
+		}
+	}
+	} 
+	for(i=0;i<n;i++){
+		for(j=0;j<n;j++){
+		printf("%d ",B[i][j]);
+		count++;
+		if(count%m==0){
+			printf("\n");
+		} 
+		}
+	}
+	return 0;
+} 
+*/
+//奇偶数排序 
+//int main(){
+//	int j=0,n=0,m=0,k=0,s=0,t=0;
+//	int num[50],a[50],b[50];
+//	scanf("%d",&n);
+//	int i=0;
+//while(n--){
+//	i=0;
+//do{
+//	scanf("%d",&num[i]);
+//	i++;
+//}while(getchar()==' ');
+//m=i;
+//for(i=0;i<m-1;i++){
+//	for(j=i+1;j<m;j++){
+//			if(num[i]>num[j]){
+//				t=num[i];
+//				num[i]=num[j];
+//				num[j]=t;
+//	}
+//}
+//}
+//for(j=0,k=0,s=0;j<m;j++){
+//	if(num[j]%2!=0){
+//		a[k]=num[j];
+//		k++;
+// }
+// else{
+// 	b[s]=num[j];
+// 	s++;
+// }
+//}
+// for(int c=0;c<k;c++){
+// 	printf("%d ",a[c]);
+// }
+// for(int d=0;d<s;d++){
+// 	printf("%d ",b[d]);
+// }
+//	printf("\n");
+//	for(i=0;i<50;i++){
+//	num[i]=0;
+//	a[i]=0;
+//	b[i]=0;
+//}
+//}
+//
+//	return 0;
+//}
+
+//查找字符串
+//思路：先在字符串里面找SCU找到之后，在里面定位，确保在这之前，以及在这之后全部都为A 
+/*
+int main(){
+	int m,flag=0,flag1=0,flag2=0;//m为数据组数
+	char str[100];
+	char a[4]="SCU";
+	scanf("%d",&m);
+	while (m--){
+		flag1=0,flag2=0;//每次循环后，判断标准要重新赋值 
+		scanf("%s",str);
+		if(strstr(str,a)==0) printf("NO\n");
+		else if(strstr(str,a)!=0){
+			for(int i=0;i<strlen(str);i++){
+				if(str[i]=='S'){
+				 flag=i;
+				 break;
+				 }
+			}
+			for(int j=flag-1;j>0;j--){
+				if(str[j]!='A')
+				 flag1=1; 
+			}
+			for(int k=flag+3;k<strlen(str);k++){
+				if(str[k]!='A') flag2=1;
+			}
+		
+		if(flag1==1||flag2==1){
+			printf("NO\n");
+		}
+		else printf("YES\n");
+	}
+	
+}
+	return 0;
+} 
+
+*/ 
+//电子表格
+//思路：输入二维数组；再用一维数组接收命令，结果再用if来判断，进行相关操作
+int main(){
+	int m,n,i=0,j=0,x,y,p,tempt;
+	char a[20][20];
+	char mand[3];
+	scanf("%d%d",&m,&n);
+	
+	for(i=0;i<m;i++){
+		for(j=0;j<n;j++){
+			scanf("%d",&a[i][j]);
+		}
+	}
+	scanf("%d",&p);//p为操作次数
+	while(p--){
+	scanf("%s",mand);
+	//行交换 
+	if(strcmp(mand,"SR")==0){
+		scanf("%d%d",&x,&y);
+		for(i=0;i<n;i++){//从左到右遍历 
+			tempt=a[x-1][i];
+			a[x-1][i]=a[y-1][i];
+			a[y-1][i]=tempt;
+		}
+	}
+	//列交换 
+	else if(strcmp(mand,"SC")==0){
+		scanf("%d%d",&x,&y);
+		for(i=0;i<m;i++){//从上到下遍历 
+			tempt=a[i][x-1];
+			a[i][x-1]=a[i][y-1];
+			a[i][y-1]=tempt;
+	}
+   }
+   //删除一行 
+   else if(strcmp(mand,"DR")==0){//上一行的等于下一行的，从删除的那行开始 
+   scanf("%d",&x);
+   	for(i=x;i<m;i++){
+   		for(j=0;j<n;j++){
+   			a[i-1][j]=a[i][j];
+		   }
+	   }
+	   m--; 
+   }
+   //删除一列 
+   else if(strcmp(mand,"DC")==0){//左边的等于右边的，从删除的那一列开始 
+   scanf("%d",&x);
+   for(i=x;i<n;i++){
+   		for(j=0;j<m;j++){
+   			a[j][i-1]=a[j][i];
+		   }
+	   } 
+	   n--; 
+   }
+   //增添一行 
+   else if(strcmp(mand,"IR")==0){//先从插入那行开始，下移后再将原本那行置0； 
+   scanf("%d",&x);
+   		for(i=m;i>=x;i--){
+   		for(j=0;j<n;j++){
+   			a[i][j]=a[i-1][j];
+		   }
+	   }
+	   
+	   m++;
+	   for(j=0;j<n;j++) 
+	   {a[x-1][j]=0;} 
+	   
+   }
+   //增添一列 
+    else if(strcmp(mand,"IC")==0){//先从插入那行开始，下移后再将原本那行置0； 
+    scanf("%d",&x);
+   		for(i=n;i>=x;i--){
+   		for(j=0;j<m;j++){
+   			a[j][i]=a[j][i-1];
+		   }
+	   }
+	   n++;
+	   for(j=0;j<m;j++) 
+	   {
+	   a[j][x-1]=0;}
+	   
+   }
+   
+}
+int count1=0;
+for(i=0;i<m;i++){
+	for(j=0;j<n;j++){
+		printf("%d ",a[i][j]);
+		count1++;
+		if(count1%n==0){
+			printf("\n");
+		}
+	}
+}
+	return 0;
+} 
+
+
+
+
+
+
+
+
 
 
 
